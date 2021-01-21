a770d2332f3aa2b0bfac51099c30fd7f495079c3
Kooma-sama <72069109+Kooma-sama@users.noreply.github.com>
2021-01-22 00:07:11 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,9 +1,5 @@
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="">
-  <meta name="author" content="Anony-mousse">
-  <meta name="generator" content="Hugo 0.79.0">
+
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
   <title>L'escroc qui rit</title>
 
@@ -14,21 +10,6 @@
   <!-- Bootstrap core CSS -->
 <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
 
-  <style>
-    .bd-placeholder-img {
-      font-size: 1.125rem;
-      text-anchor: middle;
-      -webkit-user-select: none;
-      -moz-user-select: none;
-      user-select: none;
-    }
-
-    @media (min-width: 768px) {
-      .bd-placeholder-img-lg {
-        font-size: 3.5rem;
-      }
-    }
-  </style>
 
 
   <!-- Custom styles for this template -->
