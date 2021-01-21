505ed70019e8d8440ca06bb487dadaa5c92f76e6
Axelle-ponthieu <axelleponthieu4@gmail.com>
2021-01-21 18:19:54 +0100

    voila
    voila

--- header.php
+++ header.php
@@ -1,79 +1,82 @@
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Anony-mousse">
-    <meta name="generator" content="Hugo 0.79.0">
-    <title>L'escroc qui rit</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="Anony-mousse">
+  <meta name="generator" content="Hugo 0.79.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <title>L'escroc qui rit</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/product/">
+  <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/product/">
 
-    
 
-    <!-- Bootstrap core CSS -->
+
+  <!-- Bootstrap core CSS -->
 <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
+  <style>
+    .bd-placeholder-img {
+      font-size: 1.125rem;
+      text-anchor: middle;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      user-select: none;
+    }
+
+    @media (min-width: 768px) {
+      .bd-placeholder-img-lg {
+        font-size: 3.5rem;
       }
+    }
+  </style>
 
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
 
-    
-    <!-- Custom styles for this template -->
-    <link href="product.css" rel="stylesheet">
-  </head>
-
-  <body>
-
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <meta charset="UTF-8" />
-    <title>Document</title>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+  <!-- Custom styles for this template -->
+  <link href="product.css" rel="stylesheet">
 </head>
 
+<body>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <meta charset="UTF-8" />
+  <title>Document</title>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+<div class="container-fluid">
+  <a class="navbar-brand" href="#">L'escroc qui rit</a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Escroqueries d'argent</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Escroqueries d'identité</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Escroqueries de réputation</a>
+      <li class="nav-item">
+          <a class="nav-link" href="#">Escroqueries personnelles</a>
+      </li>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Dropdown
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <li><a class="dropdown-item" href="#">Action</a></li>
+          <li><a class="dropdown-item" href="#">Another action</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="#">Something else here</a></li>
+        </ul>
+      </li>
+    </ul>
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</div>
+</nav>
+</head>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
     <div class="col-md-5 p-lg-5 mx-auto my-5">
       <h1 class="display-4 fw-normal">L'escroc qui rit</h1>
-      <p class="lead fw-normal">L'équipe de l'escroc qui rit vous propose de vous prêter main forte pour vos escroqueries en tout genre. Nous déployons plusieurs catégories de services. Demandez votre devis !</p>
+      <p class="lead fw-normal">L'equipe de l'escroc qui rit vous propose de vous preter main forte pour vos escroqueries en tout genre. Nous deployons plusieurs categories de services. Demandez votre devis !</p>
       <a class="btn btn-outline-secondary" href="pagedevis.php">Devis</a>
     </div>
     <div class="product-device shadow-sm d-none d-md-block"></div>
@@ -12,7 +12,7 @@
   </div>
 
   <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-    <div class="bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+    <div class="bg-warnings me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
       <div class="my-3 py-3">
         <h2 class="display-5">Escroquerie d'argent</h2>
         <p class="lead">Les fin de mois sont difficiles ? Nous vous proposons diverses ecroqueries qui vous permettrons de vous reposer sur vos lauriers. </p>
@@ -22,7 +22,7 @@
     <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
       <div class="my-3 p-3">
         <h2 class="display-5">Escroquerie de réputation</h2>
-        <p class="lead">Des problèmes de reputation ? Ou bien des envies de voir celle des autres changer ? Nous avons tous les outils pour vous aider dans ces tâches.</p>
+        <p class="lead">Des problemes de reputation ? Ou bien des envies de voir celle des autres changer ? Nous aurons tous les outils pour vous aider dans ces taches.</p>
       </div>
       <div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
     </div>
@@ -32,14 +32,14 @@
     <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
       <div class="my-3 p-3">
         <h2 class="display-5">Escorquerie d'identité</h2>
-        <p class="lead">Quoi de plus simple pour vivre la vie de votre voisin qui part tous les mois en vacances que de prendre sa place ? Travailler pour arriver à ses fins ne sera désormais plus necessaire, profitez du succès des autres !</p>
+        <p class="lead">Quoi de plus simple pour vivre la vie de votre voisin qui part tous les mois en vacances que de prendre sa place ? Travailler pour obtenir � ses fins ne sera d�sormais plus necessaire, profitez du succes des autres !</p>
       </div>
       <div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
     </div>
-    <div class="bg-primary me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+    <div class="bg-warning me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
       <div class="my-3 py-3">
         <h2 class="display-5">Escroquerie Service Personnel</h2>
-        <p class="lead">Nous vous proposons enfin toutes les possibilités pour démarrer une nouvelle vie. Faire disparaître vos traces, simuler votre mort et vous créer une nouvelle identité, vous aurez toutes les cartes en mains pour démarrer votre nouvelle vie. </p>
+        <p class="lead">Nous vous proposons enfin toutes les possibilités pour démarrer une nouvelle vie. Faire dispara�tre vos traces, simuler votre mort et vous cr�er une nouvelle identite, vous aurez toutes les cartes en mains pour d�marrer votre nouvelle vie. </p>
       </div>
       <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
     </div>
