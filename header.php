d66e6b4e8a4881829d6b7cb04e3980b4d2a837d2
Kooma-sama <72069109+Kooma-sama@users.noreply.github.com>
2021-01-21 20:04:53 +0100

    Update header.php

--- header.php
+++ header.php
@@ -33,9 +33,6 @@
 
   <!-- Custom styles for this template -->
   <link href="product.css" rel="stylesheet">
-</head>
-
-<body>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
   <meta charset="UTF-8" />
@@ -80,3 +77,4 @@
 </div>
 </nav>
 </head>
+<body>
