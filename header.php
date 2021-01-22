cfb41e58e3fe44e4f0a18cacda344a5bc2bb719f
Axelle-ponthieu <axelleponthieu4@gmail.com>
2021-01-22 17:11:23 +0100

    Update header.php

--- header.php
+++ header.php
@@ -42,7 +42,7 @@
   <title>Document</title>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
 <div class="container-fluid">
-  <a class="navbar-brand" href="#">L'escroc qui rit</a>
+  <a class="navbar-brand" href="inedx.php">L'escroc qui rit</a>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
